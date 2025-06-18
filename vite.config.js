c9c69fcab4ccf11753bbecc2d2590bb27c0465f3
ritesh-avkalan <ritesh.avkalan@gmail.com>
2025-06-18 13:54:07 +0530

    fix: update asset paths for deployment in index.html and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "./", // ✅ This is important
+  base: '/ritesh-portfolio/', // ✅ This is important
   plugins: [tailwindcss()],
   build: {
     outDir: "docs", // 👈 Output goes here
